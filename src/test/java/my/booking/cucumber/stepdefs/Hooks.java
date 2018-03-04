1c9e418109579cc72a416e62c130655a76449850
Sergey Shakur <sergejs.sakurs@4finance.com>
2018-03-04 20:11:30 +0200

    pretty report generation after tests execution with Gradle + added taking screenshot on scenario failure

--- src/test/java/my/booking/cucumber/stepdefs/Hooks.java
+++ src/test/java/my/booking/cucumber/stepdefs/Hooks.java
@@ -1,8 +1,11 @@
 package my.booking.cucumber.stepdefs;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
 import com.google.inject.Inject;
+import cucumber.api.Scenario;
 import cucumber.api.java.After;
 
 public class Hooks {
@@ -11,7 +14,16 @@ public class Hooks {
     private WebDriver webDriver;
 
     @After
-    public void tearDown() {
+    public void tearDown(Scenario scenario) {
+        if (scenario.isFailed()) {
+            embedScreenshot(scenario);
+        }
         webDriver.close();
     }
+
+    private void embedScreenshot(Scenario scenario) {
+        final byte[] screenshotBytes = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.BYTES);
+        scenario.embed(screenshotBytes, "image/png");
+    }
+
 }
